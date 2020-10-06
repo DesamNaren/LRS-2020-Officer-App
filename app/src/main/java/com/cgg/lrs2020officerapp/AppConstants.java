6058c5f53d9ffdc5e2956de2fde4a2026d5f0193
desam <desamnarendrakumar@gmail.com>
2020-10-06 16:56:32 +0530

    Project Dependencies

---
+++ app/src/main/java/com/cgg/lrs2020officerapp/AppConstants.java
@@ -0,0 +1,5 @@
+package com.cgg.lrs2020officerapp;
+
+public class AppConstants {
+    public static final String SHARED_PREF="com.cgg.lrs.preferences";
+}
---
+++ app/src/main/java/com/cgg/lrs2020officerapp/application/LRSApplication.java
@@ -0,0 +1,61 @@
+package com.cgg.lrs2020officerapp.application;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import androidx.multidex.MultiDex;
+import androidx.multidex.MultiDexApplication;
+
+import com.cgg.lrs2020officerapp.AppConstants;
+import com.google.gson.Gson;
+
+public class LRSApplication extends MultiDexApplication {
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private Gson gson;
+    private Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        MultiDex.install(this);
+    }
+
+    public static LRSApplication get(Context context) {
+        return (LRSApplication) context.getApplicationContext();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+
+    public SharedPreferences getPreferences() {
+        if (sharedPreferences == null) {
+            sharedPreferences = getSharedPreferences(AppConstants.SHARED_PREF, MODE_PRIVATE);
+        }
+        return sharedPreferences;
+    }
+
+    public SharedPreferences.Editor getPreferencesEditor() {
+        if (editor == null) {
+            editor = getPreferences().edit();
+        }
+        return editor;
+    }
+
+    public Gson getGson() {
+        if (gson == null) {
+            gson = new Gson();
+        }
+        return gson;
+    }
+
+    public Context getContext() {
+        if (context == null) {
+            context = getApplicationContext();
+        }
+        return context;
+    }
+}
