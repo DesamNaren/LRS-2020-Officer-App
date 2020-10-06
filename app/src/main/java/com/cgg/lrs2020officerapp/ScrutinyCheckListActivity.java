b2d25d46412b609d8a9f67d2ae02c1521976a3b3
sindhu.k <sindhu.k@cgg.gov.in>
2020-10-06 18:30:42 +0530

    Scrutiny screen

---
+++ app/src/main/java/com/cgg/lrs2020officerapp/ScrutinyCheckListActivity.java
@@ -0,0 +1,14 @@
+package com.cgg.lrs2020officerapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScrutinyCheckListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrutiny_check_list);
+    }
+}
