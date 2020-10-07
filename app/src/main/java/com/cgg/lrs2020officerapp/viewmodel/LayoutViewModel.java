7adf8121f2b96dbadbb46683c59b8c83a0619997
Niharika <nihapagudala@gmail.com>
2020-10-07 17:55:50 +0530

    application details commit

--- app/src/main/java/com/cgg/lrs2020officerapp/viewmodel/LayoutViewModel.java
+++ app/src/main/java/com/cgg/lrs2020officerapp/viewmodel/LayoutViewModel.java
@@ -12,10 +12,8 @@ import com.cgg.lrs2020officerapp.R;
 import com.cgg.lrs2020officerapp.error_handler.ErrorHandlerInterface;
 import com.cgg.lrs2020officerapp.model.applicantDetails.ApplicantDetailsReq;
 import com.cgg.lrs2020officerapp.model.applicantDetails.ApplicantDetailsRes;
-import com.cgg.lrs2020officerapp.model.land.LandDetailsResponse;
-import com.cgg.lrs2020officerapp.model.recommend.RecommendDetailsResponse;
-import com.cgg.lrs2020officerapp.model.road.RoadDetailsResponse;
 import com.cgg.lrs2020officerapp.network.LRSService;
+import com.cgg.lrs2020officerapp.utils.CustomProgressDialog;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,40 +22,45 @@ import retrofit2.Response;
 public class LayoutViewModel extends AndroidViewModel {
     private Context context;
     private MutableLiveData<ApplicantDetailsRes> applicantDetailsResMutableLiveData;
-     private ErrorHandlerInterface errorHandlerInterface;
+    private ErrorHandlerInterface errorHandlerInterface;
+    private CustomProgressDialog customProgressDialog;
+
     public LayoutViewModel(Context context, Application application) {
         super(application);
         this.context = context;
         errorHandlerInterface = (ErrorHandlerInterface) context;
-        applicantDetailsResMutableLiveData=new MutableLiveData<>();
-
+        customProgressDialog = new CustomProgressDialog(context);
+        applicantDetailsResMutableLiveData = new MutableLiveData<>();
     }
 
 
-    public LiveData<ApplicantDetailsRes> getRoadDetailsResponse(ApplicantDetailsReq applicantDetailsReq) {
-        if (applicantDetailsResMutableLiveData != null) {
-            getApplicantDetailsResponseCall(applicantDetailsReq);
-        }
+    public LiveData<ApplicantDetailsRes> getApplicantDetailsResponse() {
+        applicantDetailsResMutableLiveData = new MutableLiveData<>();
         return applicantDetailsResMutableLiveData;
     }
-    private void getApplicantDetailsResponseCall(ApplicantDetailsReq applicantDetailsReq) {
+
+    public void getApplicantDetailsResponseCall(ApplicantDetailsReq request) {
+        customProgressDialog.show();
         LRSService lrsService = LRSService.Factory.create();
-        lrsService.getApplicantDetailsResponse(applicantDetailsReq).enqueue(new Callback<ApplicantDetailsRes>() {
-            @Override
-            public void onResponse(@NonNull Call<ApplicantDetailsRes> call, @NonNull Response<ApplicantDetailsRes> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    applicantDetailsResMutableLiveData.setValue(response.body());
-                }else {
-                    errorHandlerInterface.handleError(context.getString(R.string.server_not), context);
-                }
-            }
+        lrsService.getApplicantDetailsResponse(request)
+                .enqueue(new Callback<ApplicantDetailsRes>() {
+                             @Override
+                             public void onResponse(@NonNull Call<ApplicantDetailsRes> call, @NonNull Response<ApplicantDetailsRes> response) {
+                                 customProgressDialog.dismiss();
+                                 if (response.isSuccessful() && response.body() != null) {
+                                     applicantDetailsResMutableLiveData.setValue(response.body());
+                                 } else {
+                                     errorHandlerInterface.handleError(context.getString(R.string.server_not), context);
+                                 }
+                             }
 
-            @Override
-            public void onFailure(@NonNull Call<ApplicantDetailsRes> call, @NonNull Throwable t) {
-                errorHandlerInterface.handleError(t, context);
-            }
-        });
+                             @Override
+                             public void onFailure(@NonNull Call<ApplicantDetailsRes> call, @NonNull Throwable t) {
+                                 customProgressDialog.dismiss();
+                                 errorHandlerInterface.handleError(t, context);
+                             }
+                         }
+                );
     }
-
 
 }
