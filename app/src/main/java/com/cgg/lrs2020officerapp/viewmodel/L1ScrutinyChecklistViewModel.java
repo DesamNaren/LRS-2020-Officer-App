17ef906bb75bea1b7f37e2a01653a69d64b8a264
Niharika <nihapagudala@gmail.com>
2020-11-02 14:16:09 +0530

    ui changes

--- app/src/main/java/com/cgg/lrs2020officerapp/viewmodel/L1ScrutinyChecklistViewModel.java
+++ app/src/main/java/com/cgg/lrs2020officerapp/viewmodel/L1ScrutinyChecklistViewModel.java
@@ -13,6 +13,7 @@ import com.cgg.lrs2020officerapp.error_handler.ErrorHandlerInterface;
 import com.cgg.lrs2020officerapp.model.l1ScrutinyCheckList.L1ScrutinityResponse;
 import com.cgg.lrs2020officerapp.model.l1ScrutinyCheckList.L1ScrutinyChecklistRequest;
 import com.cgg.lrs2020officerapp.network.LRSService;
+import com.cgg.lrs2020officerapp.utils.CustomProgressDialog;
 import com.google.gson.Gson;
 
 import retrofit2.Call;
@@ -20,15 +21,18 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class L1ScrutinyChecklistViewModel extends AndroidViewModel {
+
     private Context context;
     private MutableLiveData<L1ScrutinityResponse> scrutinityResponseMutableLiveData;
     private ErrorHandlerInterface errorHandlerInterface;
+    private CustomProgressDialog customProgressDialog;
+
     public L1ScrutinyChecklistViewModel(Context context, Application application) {
         super(application);
         this.context = context;
         errorHandlerInterface = (ErrorHandlerInterface) context;
         scrutinityResponseMutableLiveData =new MutableLiveData<>();
-
+        customProgressDialog = new CustomProgressDialog(context);
     }
 
 
@@ -39,11 +43,13 @@ public class L1ScrutinyChecklistViewModel extends AndroidViewModel {
         return scrutinityResponseMutableLiveData;
     }
     private void getScrutinyCheckListCall(L1ScrutinyChecklistRequest scrutinyChecklistRequest) {
+        customProgressDialog.show();
         LRSService lrsService = LRSService.Factory.create();
         String str = new Gson().toJson(scrutinyChecklistRequest);
         lrsService.getScrutinyCheckList(scrutinyChecklistRequest).enqueue(new Callback<L1ScrutinityResponse>() {
             @Override
             public void onResponse(@NonNull Call<L1ScrutinityResponse> call, @NonNull Response<L1ScrutinityResponse> response) {
+                customProgressDialog.dismiss();
                 if (response.isSuccessful() && response.body() != null) {
                     scrutinityResponseMutableLiveData.setValue(response.body());
                 }else {
@@ -53,6 +59,7 @@ public class L1ScrutinyChecklistViewModel extends AndroidViewModel {
 
             @Override
             public void onFailure(@NonNull Call<L1ScrutinityResponse> call, @NonNull Throwable t) {
+                customProgressDialog.dismiss();
                 errorHandlerInterface.handleError(t, context);
             }
         });
